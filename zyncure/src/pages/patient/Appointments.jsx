e396f7adbfc302d2acdc43263af6ccdf5bda5382
andreiesg <andreipaul.esguerra@benilde.edu.ph>
2025-05-19 19:29:28 +0800

    added sss

--- zyncure/src/pages/patient/Appointments.jsx
+++ zyncure/src/pages/patient/Appointments.jsx
@@ -1,7 +1,7 @@
 export default function Appointments() {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4 text-myHeader text-left">Appointments</h1>
+      <h1 className="text-3xl font-bold mb-4 text-myHeader text-left">Appointmentsss</h1>
     </>
   );
 }
