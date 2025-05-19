341707bf6d21122d95edce61fc274f8b2ea6f5a0
unknown <yshapapa.lim@gmail.com>
2025-05-19 12:30:31 +0800

    Edited Welcome Back to Home

--- zyncure/src/pages/patient/Home.jsx
+++ zyncure/src/pages/patient/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4 text-myHeader text-left">Welcome Back!</h1>
+      <h1 className="text-3xl font-bold mb-4 text-myHeader text-left">Welcome Home!</h1>
       <p className="text-zyncureOrange text-left">
         Here are some of your weekly and monthly records to help you track your health.
       </p>
