bf633b761be3408b97cc02e966995a2f118b31a1
ianmanalo-2104 <130700014+ianmanalo21@users.noreply.github.com>
2025-06-18 16:15:33 +0800

    ha

--- zyncure/src/pages/patient/Tracking.jsx
+++ zyncure/src/pages/patient/Tracking.jsx
@@ -5,6 +5,7 @@ import {
 } from 'lucide-react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { ShareButton } from "../../components/ShareButton.jsx";
 import { supabase } from '../../client';
 
 const PeriodTracker = () => {
